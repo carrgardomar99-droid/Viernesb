34fe5a55eb68dcee9d6157ca2bfb2ad51ef4ca5c
carrgardomar99-droid <carrgardomar99@gmail.com>
2026-05-10 21:45:58 -0600

    Actualizar cerebro.py

--- cerebro.py
+++ cerebro.py
@@ -7,3 +7,11 @@ if mision == "ENTRENAR":
     print("Preparando la armadura... ¡A darle con todo!")
 else:
     print("Modo de ahorro de energía activado. Disfrute su descanso, señor.")
+
+humor = input("¿Cómo se siente hoy, señor? ")
+
+if humor == "ABURRIDO":
+    print("¿Sabe qué hace un robot al salir de vacaciones?")
+    print("¡Se va a la playa a cargar las baterías! Jajaja.")
+else:
+    print("Me alegra que se sienta " + humor + ". ¡Vamos por un gran día!")
