d58aabea7fd452737d9782883e0fd2841a40b8ac
carrgardomar99-droido <carrgardomar99@gmail.com>
2026-05-16 20:35:45 -0600

    Tarea 2 Completada - Módulo de Veracidad y Búsqueda Inteligente

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-// Servidor de fondo de YARBIS
+// Service Worker básico para permitir la instalación de la PWA
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    // Permite que la app cargue de manera normal mediante internet
+    event.respondWith(fetch(event.request));
+});
